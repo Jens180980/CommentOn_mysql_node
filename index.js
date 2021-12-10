26af1cab4c1ad9dd888108e197713c65f88e6dbe
jens180980 <jens180980@gmail.com>
2021-12-10 13:23:37 +0100

    add comments to database succesfull

---
+++ index.js
@@ -0,0 +1,36 @@
+
+const express = require('express')
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+const encoder = bodyParser.urlencoded()
+
+const app = express()
+app.use('/assets/css', express.static('assets/css'))
+
+const connection = mysql.createConnection({
+    host    :   'localhost',
+    user    :   'root',
+    password:   'ger140793',
+    database:   'comment_on'
+})
+
+connection.connect( err => {
+    if(err) throw err
+    else console.log('Database connection succesfull!');
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post("/",encoder, function(req,res){
+    const author = req.body.author;
+    var comment = req.body.comment;
+
+    connection.query(`insert into comment(author, comment) values ('${author}', '${comment}')`, function(err,results,fields){
+        if(err) throw err
+        else console.log('comment added to database');
+    })
+})
+
+app.listen(4000)
